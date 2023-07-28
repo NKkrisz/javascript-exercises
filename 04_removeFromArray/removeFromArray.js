c78a1f62e50757f0ec0572a43816e5929efbb8cd
NKkrisz <nkkrisz2005@gmail.com>
2023-07-28 19:24:10 +0200

    Completed Fundamentals Part 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arr, remove, remove2=undefined, remove3=undefined, remove4=undefined) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i] === remove || arr[i] === remove2 || arr[i] === remove3 || arr[i] === remove4){
+//             arr.splice(i, 1)
+//             i = 0
+//         }
+//     }
+//     return arr;
+// };
 
+const removeFromArray = function(arr, ...remove) {
+    for (let i = arr.length -1; i > -1; i--) {
+        if(remove.includes(arr[i])){
+            arr.splice(i, 1)
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR";
+    }
+        
+    let sum = 0;
+    let currentNumber = num1;
+    let untilNumber = num2;
 
+    if(num1 > num2){
+        currentNumber = num2;
+        untilNumber = num1;
+    }
+
+    while(currentNumber < untilNumber+1){
+        sum += currentNumber;
+        currentNumber++;
+    }
+
+    if(sum < 0) return "ERROR";
+
+    return sum;
 };
 
 // Do not edit below this line
