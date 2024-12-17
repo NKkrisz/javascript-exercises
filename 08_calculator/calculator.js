4221fded1d557c116f8c38cd085f8faddc03611e
NKkrisz <nkkrisz2005@gmail.com>
2024-12-17 23:37:56 +0100

    Completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  if(typeof(num1) == "number" && typeof(num2) == "number") return num1 + num2
+  return "ERROR"
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	if(typeof(num1) == "number" && typeof(num2) == "number") return num1 - num2
+  return "ERROR"
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0
+	arr.forEach((num) => result += num)
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = arr[0]
+  for (let index = 1; index < arr.length; index++) {
+    result *= arr[index]
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	if(typeof(num1) == "number" && typeof(num2) == "number") return num1 ** num2
+  return "ERROR"
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) return 1
+	let result = num
+  for(let i = num-1; i > 0; i--){
+    result = result * i
+  }
+
+  return result
 };
 
 // Do not edit below this line
